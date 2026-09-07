5877f9dc69e398f3623a0f2956169d72b821c0ee
Ngan <contact.,nghiemngan@gmail.com>
2026-09-07 16:11:25 +0200

    added new file

--- array.c
+++ array.c
@@ -1,3 +1,22 @@
-//
-// Created by Admin on 9/7/2026.
-//
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter (N): ");
+    scanf("%d", &n);
+
+    int arr[n]; // (VLA - C99)
+
+    // Enter each element/ i run from 0 to n - 1
+    for (int i = 0; i < n; i++) {
+        printf("arr[%d] = ", i);
+        scanf("%d", &arr[i]); // storage i
+    }
+
+    // Print to test
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
